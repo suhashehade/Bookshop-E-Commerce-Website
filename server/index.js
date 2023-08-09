ab1a4d1a9180bdc9e2f4cbee5eb2058c16abe414
Suha523 <suhashehadeh123@gmail.com>
2023-08-10 01:13:07 +0300

    fix upload files

--- server/index.js
+++ server/index.js
@@ -22,9 +22,9 @@ mongoose.connect(
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(upload());
-app.use("/uploads/covers", express.static("uploads/covers"));
-app.use("/uploads/authors", express.static("uploads/authors"));
+// app.use(upload());
+// app.use("/uploads/covers", express.static("uploads/covers"));
+// app.use("/uploads/authors", express.static("uploads/authors"));
 app.use("/", api);
 app.listen(PORT, function () {
   console.log(`server is listen on port: ${PORT}`);

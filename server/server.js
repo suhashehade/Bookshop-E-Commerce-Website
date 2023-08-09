bf768e2d84fc9f9abf565c086415091cf26a7fd3
Suha523 <suhashehadeh123@gmail.com>
2023-08-09 20:06:44 +0300

    add vercel.json

--- server/server.js
+++ server/server.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const upload = require('express-fileupload');
-const api = require('./server/routes/api');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const upload = require("express-fileupload");
+const api = require("./server/routes/api");
+const cors = require("cors");
 const app = express();
 const PORT = 4000;
 
-mongoose.connect('mongodb://localhost/bookshop');
+mongoose.connect(
+  "mongodb+srv://Suha:988456321_cse@cluster0.p47c2t1.mongodb.net/bookshop?retryWrites=true&w=majority",
+);
+
+app.use(
+  cors({
+    origin: ["https://knowledge-journey-bookshop.vercel.app"],
+    methods: ["POST", "GET", "DELETE", "PUT"],
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 app.use(upload());
-app.use('/uploads/covers', express.static('uploads/covers'));
-app.use('/uploads/authors', express.static('uploads/authors'));
-app.use('/', api);
-app.listen(PORT, function (){
-   console.log(`server is listen on port: ${PORT}`);
-})
-
-
+app.use("/uploads/covers", express.static("uploads/covers"));
+app.use("/uploads/authors", express.static("uploads/authors"));
+app.use("/", api);
+app.listen(PORT, function () {
+  console.log(`server is listen on port: ${PORT}`);
+});

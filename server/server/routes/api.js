d7019eed09babdf7d4c81d60555d9cdf6eb17e95
Suha523 <suhashehadeh123@gmail.com>
2023-08-09 23:51:46 +0300

    fix cors

--- server/server/routes/api.js
+++ server/server/routes/api.js
@@ -65,26 +65,6 @@ router.get("/category", async function (req, res) {
 router.post("/author", function (req, res) {
   console.log(req.body);
   res.end();
-  // let picture = req.files.picture;
-  // let picture_name = picture.name.split(".")[0] + Date.now() + ".png";
-  // console.log(picture_name);
-  // res.end();
-  // picture.mv("uploads/authors/" + picture_name, function (err) {
-  //   if (err) {
-  //     res.send(err);
-  //   } else {
-  //     res.send("file uploaded");
-  //     let authorData = {
-  //       name: req.body.name,
-  //       picture: picture_name,
-  //       email: req.body.email,
-  //       phone: req.body.phone,
-  //     };
-  //     let author = new Author(authorData);
-  //     author.save();
-  //     res.end();
-  //   }
-  // });
 });
 
 router.get("/author", async function (req, res) {

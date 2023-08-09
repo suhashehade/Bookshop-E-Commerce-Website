278080d41348f566793a8d50f9a8b9cd1aaf0b01
Suha523 <suhashehadeh123@gmail.com>
2023-08-09 22:00:42 +0300

    fix cors error

--- server/index.js
+++ server/index.js
@@ -20,6 +20,17 @@ app.use(
   }),
 );
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With",
+  );
+
+  next();
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(upload());

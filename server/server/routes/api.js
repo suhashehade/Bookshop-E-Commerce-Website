426059cc9151967d0574f2cf40e78d67fe1ef161
Suha523 <suhashehadeh123@gmail.com>
2023-08-10 13:17:47 +0300

    fix upload files

--- server/server/routes/api.js
+++ server/server/routes/api.js
@@ -69,7 +69,7 @@ router.post("/author", function (req, res) {
   let picture_name = picture.name.split(".")[0] + Date.now() + ".png";
   picture.mv("uploads/authors/" + picture_name, function (err) {
     if (err) {
-      res.status(500).send({ error: "error: uploaded failed" });
+      res.status(500).send({ error: err });
     } else {
       res.send("file uploaded");
       let authorData = {

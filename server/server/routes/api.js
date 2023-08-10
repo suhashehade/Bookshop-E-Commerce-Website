ac9f75553dc15ae8208571246dc063989695affd
Suha523 <suhashehadeh123@gmail.com>
2023-08-10 13:54:23 +0300

    fix upload files

--- server/server/routes/api.js
+++ server/server/routes/api.js
@@ -67,21 +67,22 @@ router.get("/category", async function (req, res) {
 router.post("/author", function (req, res) {
   let picture = req.files.picture;
   let picture_name = picture.name.split(".")[0] + Date.now() + ".png";
-  picture.mv("uploads/authors/" + picture_name, function (err) {
-    if (err) {
-      res.send({ error: "this is error is occured:" + err });
-    } else {
-      res.send("file uploaded");
-      let authorData = {
-        name: req.body.name,
-        picture: picture_name,
-        email: req.body.email,
-        phone: req.body.phone,
-      };
-      let author = new Author(authorData);
-      author.save();
-    }
-  });
+  res.send(picture_name);
+  // picture.mv("uploads/authors/" + picture_name, function (err) {
+  //   if (err) {
+  //     res.send({ error: "this is error is occured:" + err });
+  //   } else {
+  //     res.send("file uploaded");
+  //     let authorData = {
+  //       name: req.body.name,
+  //       picture: picture_name,
+  //       email: req.body.email,
+  //       phone: req.body.phone,
+  //     };
+  //     let author = new Author(authorData);
+  //     author.save();
+  //   }
+  // });
 });
 
 router.get("/author", async function (req, res) {

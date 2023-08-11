5fe2e52bfb58cba939034d5f18a77facb5049e85
Suha523 <suhashehadeh123@gmail.com>
2023-08-12 01:11:06 +0300

    fix files upload

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ mongoose.connect(
 );
 
 app.use(express.json({ limit: "50mb" }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.static(path.join(__dirname, "tmp")));
 app.use(upload({ useTempFiles: true, tempFileDir: "/tmp" }));

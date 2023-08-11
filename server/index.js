ac4d1a272b149f920c14cd1c3d412bac0b058cd9
Suha523 <suhashehadeh123@gmail.com>
2023-08-11 10:15:18 +0300

    fix files upload

--- server/index.js
+++ server/index.js
@@ -3,24 +3,11 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const upload = require("express-fileupload");
-const fs = require("fs");
 const api = require("./server/routes/api");
 const cors = require("cors");
 const app = express();
 const PORT = 4000;
 
-fs.access(
-  "public/uploads/authors",
-  fs.constants.R_OK | fs.constants.W_OK,
-  (err) => {
-    if (err) {
-      console.error("Folder does not have read and write permissions:", err);
-      // You might want to handle this error accordingly
-    } else {
-      console.log("Folder has read and write permissions");
-    }
-  },
-);
 app.use(
   cors({
     origin: ["https://knowledge-journey-bookshop.vercel.app"],
@@ -37,8 +24,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(upload());
 
-app.use("/public/uploads/covers", express.static("public/uploads/covers"));
-app.use("/public/uploads/authors", express.static("public/uploads/authors"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use("/", api);
 app.listen(PORT, function () {
   console.log(`server is listen on port: ${PORT}`);
